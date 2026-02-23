4868f3b7ed84770c5f411a1cb1665265c74bb8a8
ColeFrangipane <colefrangipane05@gmail.com>
2026-02-23 17:10:38 -0600

    Add files via upload

--- lab3/geo455_lab3.js
+++ lab3/geo455_lab3.js
@@ -1,4 +1,4 @@
-const map = L.map("map").setView([45.1304462401535, -87.4931120059192] ,9.5);
+const map = L.map("map").setView([45.10617608844984, -87.6209157105656] ,9.5);
 
 var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
